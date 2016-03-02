681f757a5b3cf6c48f2a0cc02f553167e6e9c4cb
DongHyung Lee <DongHyung Lee>
2016-03-02 16:41:16 +0900

    Exception Fixed

--- Caculator_Ver2/Program.cs
+++ Caculator_Ver2/Program.cs
@@ -32,59 +32,69 @@ namespace Caculator_Ver2
 
         static void Main(string[] args)
         {
-            String Power_Method;
-            int Power_Method_Number;
 
-            String Caculate_Method;
-            int Caculate_Method_Number;
+            string Power_Method;
 
-            String input_x, input_y;
+            string Calculate_Method;
+            int Calculate_Method_Number;
+
+            string input_x, input_y;
             int x, y;
             int Remain_Result;
             int Result;
 
-            Console.WriteLine("Caculator 0.2 by SCOUTeLEE\n\n\n\n\n\n");
-            //Console.Write("계산기를 시작하시겠습니까? Y/N :");
+            Console.WriteLine("Caculator 0.2 by SCOUTLEE\n\n\n\n\n\n");
+            Console.Write("계산기를 시작하시겠습니까? Y/N :");
+            Power_Method = Console.ReadLine();
 
-
-            Console.Write("첫번째 숫자를 입력하세요: ");
-            input_x = Console.ReadLine();
-            x = int.Parse(input_x);
-
-            Console.Write("두번째 숫자를 입력하세요: ");
-            input_y = Console.ReadLine();
-            y = int.Parse(input_y);
-
-
-            Console.Write("계산방법을 선택하시요: (1)덧셈 (2)뺄셈 (3)곱셈 (4)나눗셈 => ");
-            Power_Method=Console.ReadLine();
-            Power_Method_Number = int.Parse(Power_Method);
-
-
-            switch (Power_Method_Number)
+            while (Power_Method=="y")
             {
-                case 1:
-                    Result = Plus(x, y);
-                    Console.Write("결과값: {0}\n\n", Result);
-                    break;
+                Console.Write("첫번째 숫자를 입력하세요: ");
+                input_x = Console.ReadLine();
+                x = int.Parse(input_x);
 
-               case 2:
-                    Result = Minus(x, y);
-                    Console.Write("결과값: {0}\n\n", Result);
-                    break;
+                Console.Write("두번째 숫자를 입력하세요: ");
+                input_y = Console.ReadLine();
+                y = int.Parse(input_y);
 
-                case 3:
-                    Result = Multiplex(x, y);
-                    Console.Write("결과값: {0}\n\n", Result);
-                    break;
 
-                case 4:
-                    Divide(x, y, out Result, out Remain_Result);
-                    Console.Write("결과값/나머지값: {0}/{1}\n\n", Result, Remain_Result);
-                    break;
+                    Console.Write("계산방법을 선택하시요: (1)덧셈 (2)뺄셈 (3)곱셈 (4)나눗셈 => ");
+                     Calculate_Method = Console.ReadLine();
+                     Calculate_Method_Number = int.Parse(Calculate_Method);
 
+
+                switch (Calculate_Method_Number)
+                {
+                    case 1:
+                        Result = Plus(x, y);
+                        Console.Write("결과값: {0}\n\n", Result);
+                        break;
+
+                    case 2:
+                        Result = Minus(x, y);
+                        Console.Write("결과값: {0}\n\n", Result);
+                        break;
+
+                    case 3:
+                        Result = Multiplex(x, y);
+                        Console.Write("결과값: {0}\n\n", Result);
+                        break;
+
+                    case 4:
+                        Divide(x, y, out Result, out Remain_Result);
+                        Console.Write("결과값/나머지값: {0}/{1}\n\n", Result, Remain_Result);
+                        break;
+
+                    default:
+                        Console.WriteLine("\n\n잘못된 번호입니다.");
+                        break;
+                }
+                Console.Write("계속 계산 하시겠습니까? Y/N => ");
+                Power_Method = Console.ReadLine();
+
+                if (Power_Method == "Y")
+                    break;
             }
-            
         }
     }
 }
